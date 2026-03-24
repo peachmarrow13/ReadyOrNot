23c5d3f2f73c92b44448cd852d2297539ca44754
PeachMarrow13 <kk@ss.com>
2026-03-24 14:02:46 -0400

    Changed version number

--- ReadyOrNot/DLLMain.cpp
+++ ReadyOrNot/DLLMain.cpp
@@ -2,7 +2,7 @@
 
 #define MAJORVERSION 2
 #define MINORVERSION 4
-#define PATCHVERSION 4
+#define PATCHVERSION 6
 
 static const std::pair<const char*, std::string> BoneOptions[] = {
 	{"Head", BoneList.HeadBone},
